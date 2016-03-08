d21b5c309a110471079fe728dc23f1d417245369
KageKirin <kagekirin+khutil@gmail.com>
2016-03-08 14:16:15 +0900

    added unit test for logging

--- unittests/core.ut.cpp
+++ unittests/core.ut.cpp
@@ -1,1 +1,24 @@
-﻿
+﻿#include "khutils/logging.hpp"
+
+#include <bandit/bandit.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace bandit;
+using namespace khutils;
+
+
+go_bandit([]() {
+	//////////////////////////////////////////////////////////////////////////
+	// logger
+	describe("logging styles", []() {
+		it("log", []() {
+			khutils::logger::debug() << "Debug message" << std::endl;
+			khutils::logger::log() << "Log message" << std::endl;
+			khutils::logger::warn() << "Warning message" << std::endl;
+			khutils::logger::error() << "Error message" << std::endl;
+			AssertThat(true, Equals(true));
+		});
+	});
+});
