472e11c06a6e9f3a5e3bd6d784b19b2075d8ff96
KageKirin <kagekirin+khutil@gmail.com>
2016-03-16 17:49:03 +0900

    added algorithm_index.hpp for some C++11 algorithms that provide the current elements index as well

---
+++ include/khutils/algorithm_index.hpp
@@ -0,0 +1,69 @@
+﻿#ifndef KHUTILS_ALGORITHM_INDEXED_HPP_INC
+#define KHUTILS_ALGORITHM_INDEXED_HPP_INC
+
+#include <algorithm>
+#include <functional>
+#include <pythonic/enumerate.h>
+
+namespace khutils
+{
+	// naive C++ implementation
+	template <class InputIt, class BinaryOperation>
+	void for_each_indexed0(InputIt first, InputIt last, BinaryOperation binary_op)
+	{
+		for (auto it = first; it != last; ++it)
+		{
+			binary_op(*it, std::distance(it, first));
+		}
+	}
+
+	// naive C++11 implementation
+	template <class InputIt, class BinaryOperation>
+	void for_each_indexed(InputIt first, InputIt last, BinaryOperation binary_op)
+	{
+		size_t index = 0;
+		std::for_each(first, last, [&index, &binary_op](typename InputIt::value_type& value) {
+			binary_op(value, index);
+			++index;
+		});
+	}
+
+	// using pythonic.enumerate
+	template <class _Collection, class BinaryOperation>
+	void for_each_indexed_pythonic(_Collection collection, BinaryOperation binary_op)
+	{
+		for (auto vp : pythonic::enumerate(collection))
+		{
+			binary_op(vp.second, vp.first);
+		}
+	}
+
+	// naive C++ implementation
+	template <class InputIt, class OutputIt, class BinaryOperation>
+	OutputIt transform_indexed0(InputIt first, InputIt last, OutputIt d_first, BinaryOperation binary_op)
+	{
+		auto d_it = d_first;
+		for (auto it = first; it != last; ++it)
+		{
+			size_t index = std::distance(it, first);
+			d_it		 = d_first + index;
+			*d_it		 = binary_op(*it, index);
+		}
+		return d_it;
+	}
+
+	// naive C++11 implementation
+	template <class InputIt, class OutputIt, class BinaryOperation>
+	OutputIt transform_indexed(InputIt first, InputIt last, OutputIt d_first, BinaryOperation binary_op)
+	{
+		size_t index = 0;
+		return std::transform(first, last, d_first, [&index, &binary_op](typename InputIt::value_type & value) -> typename OutputIt::value_type {
+			auto v = binary_op(value, index);
+			++index;
+			return v;
+		});
+	}
+
+}	// namespace khutils
+
+#endif	//! KHUTILS_ALGORITHM_INDEXED_HPP_INC
