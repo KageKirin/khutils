82084934dec32fa87377556bb3fe11f7a86b9b02
KageKirin <kagekirin+khutil@gmail.com>
2016-04-15 10:09:47 +0900

    renamed static_strings to assets

--- unittests/main.ut.cpp
+++ unittests/main.ut.cpp
@@ -1,8 +1,11 @@
 ﻿#include <bandit/bandit.h>
+#include <cstdlib>
 
 using namespace bandit;
 
 int main(int argc, char* argv[])
 {
+	setenv("XcodeColors", "YES", 0);	// Enables XcodeColors (you obviously have to install it too)
+
 	return bandit::run(argc, argv);
 }
