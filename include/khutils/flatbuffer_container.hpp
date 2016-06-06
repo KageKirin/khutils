1c7b035ce907a7a5a5e768f7b593d40881a4504e
KageKirin <kagekirin+khutil@gmail.com>
2016-06-06 11:34:41 +0900

    implemented FlatbufferContainer to keep flatbuffer data buffer and accessor functions together

---
+++ include/khutils/flatbuffer_container.hpp
@@ -0,0 +1,50 @@
+﻿#ifndef KHUTILS_FLATBUFFER_CONTAINER_HPP_INC
+#define KHUTILS_FLATBUFFER_CONTAINER_HPP_INC
+
+#include <cstdint>
+#include <vector>
+
+namespace khutils
+{
+	template <typename _T>
+	class FlatbufferContainer
+	{
+	public:
+		typedef _T FlatbufferType;
+
+	private:
+		std::vector<uint8_t> m_buffer;	//< holds the flatbuffer data
+										  //< technically, this could be improved with a shared_array<> if STL had it
+										  //(Boost has it, though)
+
+	public:
+		FlatbufferContainer() = delete;
+		FlatbufferContainer(const std::vector<uint8_t>& buffer) : m_buffer(buffer)
+		{
+		}
+		FlatbufferContainer(uint8_t* data, size_t length) : m_buffer(data, data + length)
+		{
+		}
+		FlatbufferContainer(const FlatbufferContainer& rhv) = default;	//: FlatbufferContainer(rhv.m_buffer){}
+		FlatbufferContainer(FlatbufferContainer&& rhv)		= default;
+		~FlatbufferContainer()								= default;
+
+		FlatbufferContainer& operator=(const FlatbufferContainer& rhv) = default;
+		FlatbufferContainer& operator=(FlatbufferContainer&& rhv) = default;
+
+		inline const FlatbufferType* ptr()
+		{
+			return flatbuffers::GetRoot<FlatbufferType>(m_buffer.data());
+		}
+
+		inline const FlatbufferType& ref()
+		{
+			auto ptr = ptr();
+			assert(ptr);
+			return *ptr;
+		}
+	};
+
+}	// namespace khutils
+
+#endif	// !KHUTILS_FLATBUFFER_CONTAINER_HPP_INC
