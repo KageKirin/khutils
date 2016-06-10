37c5f76da5ca2504d641495a9b8189efb3c13fcf
KageKirin <kagekirin+khutil@gmail.com>
2016-06-10 18:37:10 +0900

    added assertion wrapper

---
+++ include/khutils/assertion.hpp
@@ -0,0 +1,24 @@
+﻿#ifndef KHUTILS_ASSERTION_HPP_INC﻿
+#define KHUTILS_ASSERTION_HPP_INC
+
+#if defined(KHUTILS_UNITTEST)
+#include <bandit/bandit.h>	//TODO: change to header for assertion only
+
+#define KHUTILS_ASSERT(variable, value) bandit::AssertThat((variable), Equals((value)))
+#define KHUTILS_ASSERT_PTR((ptr)) bandit::AssertThat((ptr), NotEquals(nullptr))
+
+#else
+#include <cassert>
+
+#define KHUTILS_ASSERT(variable, value) assert((variable) == (value))
+#define KHUTILS_ASSERT_PTR(ptr) assert((ptr) != nullptr)
+
+// TODO: implement own assert, maybe?
+
+#endif	// defined(KHUTILS_UNITTEST)
+
+#if defined(KHUTILS_ASSERTION_IMPL)
+
+#endif	// defined (KHUTILS_ASSERTION_IMPL)
+
+#endif	// !KHUTILS_ASSERTION_HPP_INC
