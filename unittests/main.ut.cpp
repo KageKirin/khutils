1505d4ee50dedf480138417231a8274a15dadcf1
KageKirin <kagekirin+khutil@gmail.com>
2016-06-30 12:15:16 +0900

    fix for windows environment

--- unittests/main.ut.cpp
+++ unittests/main.ut.cpp
@@ -5,7 +5,9 @@ using namespace bandit;
 
 int main(int argc, char* argv[])
 {
+#ifndef _WIN32
 	setenv("XcodeColors", "YES", 0);	// Enables XcodeColors (you obviously have to install it too)
+#endif									//_WIN32
 
 	return bandit::run(argc, argv);
 }
