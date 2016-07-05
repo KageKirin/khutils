afb7a5cf2e5abf96a2395fd2ccf7e37d62774520
KageKirin <kagekirin+khutil@gmail.com>
2016-07-05 15:07:23 +0900

    asserts write message where assertion happened BEFORE new style assert added optional conventional assert (cassert) to trigger before new style assert

--- include/khutils/assertion.hpp
+++ include/khutils/assertion.hpp
@@ -1,6 +1,7 @@
 ﻿#ifndef KHUTILS_ASSERTION_HPP_INC
 #define KHUTILS_ASSERTION_HPP_INC
 
+#include <bandit/assertion_frameworks/snowhouse/snowhouse/snowhouse.h>
 #include <memory>
 
 namespace khutils
@@ -35,73 +36,128 @@ namespace khutils
 		khutils::Assert((variable), (expr), __FILE__, __LINE__);                                                       \
 	};
 
-#include "khutils/assertion.inl"
 
-#if defined(KHUTILS_ASSERTION_IMPL)
+#if defined(KHUTILS_ASSERTION_INLINE) || defined(KHUTILS_ASSERTION_IMPL)
 
-#include "khutils/assertion.hpp"
-#include "khutils/runtime_exceptions.hpp"
-
-#include <bandit/assertion_frameworks/snowhouse/snowhouse/snowhouse.h>﻿
-
-#include <bandit/assertion_frameworks/snowhouse/snowhouse/fluent/expressionbuilder.h>
-
-namespace khutils
-{
-
-	void Assert(const bool cond, const char* _file, const int _line)
-	{
-		using namespace snowhouse;
-		ConfigurableAssert<DefaultFailureHandler>::That(cond, IsTrue(), _file, _line);
-	}
-
-}	// namespace khutils
-
-#endif	// defined (KHUTILS_ASSERTION_IMPL)
-
-#if defined(KHUTILS_ASSERTION_IMPL)
-
+#include "khutils/logging.hpp"
 #include <iostream>
-
-#include <bandit/assertion_frameworks/snowhouse/snowhouse/snowhouse.h>	//﻿
 // keep this space to force order of includes
 #include <bandit/assertion_frameworks/snowhouse/snowhouse/fluent/expressionbuilder.h>
+
+//#define WITH_CASSERT
+
+#ifdef WITH_CASSERT
+#include <cassert>
+#define CASSERT(x) assert(x)
+#else
+#define CASSERT(x)
+#endif	// WITH_CASSERT
+
 
 namespace khutils
 {
 	template <typename T>
 	void Assert(const T& var, const T& val, const char* _file, const int _line)
 	{
-		using namespace snowhouse;
-		ConfigurableAssert<DefaultFailureHandler>::That(var, Is().EqualTo(val), _file, _line);
+		CASSERT(var == val);
+		try
+		{
+			using namespace snowhouse;
+			ConfigurableAssert<DefaultFailureHandler>::That(var, Is().EqualTo(val), _file, _line);
+		}
+		catch (...)
+		{
+			logger::error() << "assertion in " << _file << " at line " << _line;
+			throw;
+		}
 	}
 
 	template <typename T, typename Expression>
 	void Assert(const T& var, Expression expression, const char* _file, const int _line)
 	{
-		using namespace snowhouse;
-		ConfigurableAssert<DefaultFailureHandler>::That(var, expression, _file, _line);
+		try
+		{
+			using namespace snowhouse;
+			ConfigurableAssert<DefaultFailureHandler>::That(var, expression, _file, _line);
+		}
+		catch (...)
+		{
+			logger::error() << "assertion in " << _file << " at line " << _line;
+			throw;
+		}
 	}
 
 	template <typename T>
 	void AssertNullPtr(const T* ptr, const char* _file, const int _line)
 	{
-		using namespace snowhouse;
-		ConfigurableAssert<DefaultFailureHandler>::That(ptr, Is().EqualTo((T*)nullptr), _file, _line);
+		CASSERT(ptr == (T*)nullptr);
+		try
+		{
+			using namespace snowhouse;
+			ConfigurableAssert<DefaultFailureHandler>::That(ptr, Is().EqualTo((T*)nullptr), _file, _line);
+		}
+		catch (...)
+		{
+			logger::error() << "assertion in " << _file << " at line " << _line;
+			throw;
+		}
 	}
 
 	template <typename T>
 	void AssertValidPtr(const T* ptr, const char* _file, const int _line)
 	{
-		using namespace snowhouse;
-		ConfigurableAssert<DefaultFailureHandler>::That(ptr, Is().Not().EqualTo((T*)nullptr), _file, _line);
+		CASSERT(ptr != (T*)nullptr);
+		try
+		{
+			using namespace snowhouse;
+			ConfigurableAssert<DefaultFailureHandler>::That(ptr, Is().Not().EqualTo((T*)nullptr), _file, _line);
+		}
+		catch (...)
+		{
+			logger::error() << "assertion in " << _file << " at line " << _line;
+			throw;
+		}
 	}
 
 	template <typename T>
 	void AssertValidSharedPtr(const std::shared_ptr<T>& ptr, const char* _file, const int _line)
 	{
-		using namespace snowhouse;
-		ConfigurableAssert<DefaultFailureHandler>::That((bool)ptr, IsTrue(), _file, _line);
+		CASSERT((bool)ptr);
+		try
+		{
+			using namespace snowhouse;
+			ConfigurableAssert<DefaultFailureHandler>::That((bool)ptr, IsTrue(), _file, _line);
+		}
+		catch (...)
+		{
+			logger::error() << "assertion in " << _file << " at line " << _line;
+			throw;
+		}
+	}
+}	// namespace khutils
+
+#endif	// defined(KHUTILS_ASSERTION_INLINE) || defined(KHUTILS_ASSERTION_IMPL)
+
+
+#if defined(KHUTILS_ASSERTION_IMPL)
+
+#include "khutils/runtime_exceptions.hpp"
+
+namespace khutils
+{
+	void Assert(const bool cond, const char* _file, const int _line)
+	{
+		CASSERT(cond);
+		try
+		{
+			using namespace snowhouse;
+			ConfigurableAssert<DefaultFailureHandler>::That(cond, IsTrue(), _file, _line);
+		}
+		catch (...)
+		{
+			logger::error() << "assertion in " << _file << " at line " << _line;
+			throw;
+		}
 	}
 
 }	// namespace khutils
