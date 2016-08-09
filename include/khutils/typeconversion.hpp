e7c01520ccc0aef5ffd9b249082d3f96b3c74b7c
KageKirin <kagekirin+khutil@gmail.com>
2016-08-09 18:47:14 +0900

    overloaded boost::endian_reverse for float and double in typeconversion.hpp  khutils namespace

--- include/khutils/typeconversion.hpp
+++ include/khutils/typeconversion.hpp
@@ -1,6 +1,42 @@
 ﻿#ifndef KHUTILS_TYPECONVERSION_HPP_INC
 #define KHUTILS_TYPECONVERSION_HPP_INC
 
+#include <boost/config.hpp>
+#include <boost/cstdint.hpp>
+
+// overload function endian_reverse BEFORE including header
+namespace boost
+{
+	namespace endian
+	{
+		double endian_reverse(double x) BOOST_NOEXCEPT;
+		float endian_reverse(float x) BOOST_NOEXCEPT;
+
+	}	// namespace endian
+}	// namespace boost
+
+#include <boost/endian/conversion.hpp>
+
+namespace boost
+{
+	namespace endian
+	{
+
+		inline double endian_reverse(double x) BOOST_NOEXCEPT
+		{
+			auto rx = endian_reverse(*reinterpret_cast<uint64_t*>(&x));
+			return *reinterpret_cast<double*>(&rx);
+		}
+
+		inline float endian_reverse(float x) BOOST_NOEXCEPT
+		{
+			auto rx = endian_reverse(*reinterpret_cast<uint32_t*>(&x));
+			return *reinterpret_cast<float*>(&rx);
+		}
+
+	}	// namespace endian
+}	// namespace boost
+
 namespace khutils
 {
 	// converts provided _InT into _OutT performing reinterpret_cast on memory
@@ -23,7 +59,11 @@ namespace khutils
 		return val.t;
 	}
 
-	///
+	// importing boost::endian_reverse symbol into our namespace
+	using boost::endian::endian_reverse;
+	using boost::endian::conditional_reverse;
+
+	// TODO: overload endian_reverse for more types
 
 }	// namespace khutils
 
