0fd24a2dc36eaf5da0e36b98b15fd376d1dc2571
KageKirin <kagekirin+khutil@gmail.com>
2016-09-01 14:15:45 +0900

    added macros to overload reader/write respective single element read/write methods

---
+++ include/khutils/reader_overload.hpp
@@ -0,0 +1,25 @@
+﻿#ifndef KHUTILS_READER_OVERLOAD_HPP_INC
+#define KHUTILS_READER_OVERLOAD_HPP_INC
+
+//! usage:
+//! 1. include this file in a cpp AFTER including the reader to overload
+//! 2. use the KUTILS_OVERLOAD_READER macro to overload reader::read<> for
+//! single element
+//! 3. func must be passed enclosed in parenthesis ({ /*...*/})
+
+
+#include <boost/preprocessor/punctuation/remove_parens.hpp>
+
+#define KUTILS_OVERLOAD_READER_DECL(readerT, outT, readT)                                                              \
+	template <>                                                                                                        \
+	outT readerT::read<outT, readT>(SwapConversionFuncT<outT, readT> swapConv);
+
+#define KUTILS_OVERLOAD_READER_IMPL(readerT, outT, readT, func)                                                        \
+	template <>                                                                                                        \
+	outT readerT::read<outT, readT>(SwapConversionFuncT<outT, readT> swapConv) BOOST_PP_REMOVE_PARENS(func)
+
+namespace khutils
+{
+}	// namespace khutils
+
+#endif	// ! KHUTILS_READER_OVERLOAD_HPP_INC
---
+++ include/khutils/writer_overload.hpp
@@ -0,0 +1,25 @@
+﻿#ifndef KHUTILS_WRITER_OVERLOAD_HPP_INC
+#define KHUTILS_WRITER_OVERLOAD_HPP_INC
+
+//! usage:
+//! 1. include this file in a cpp AFTER including the writer to overload
+//! 2. use the KUTILS_OVERLOAD_WRITER macro to overload writer::write<> for
+//! single element
+//! 3. func must be passed enclosed in parenthesis ({ /*...*/})
+
+
+#include <boost/preprocessor/punctuation/remove_parens.hpp>
+
+#define KUTILS_OVERLOAD_WRITER_DECL(writerT, writeT, inT)                                                              \
+	template <>                                                                                                        \
+	void writerT::write<writeT, inT>(inT t, SwapConversionFuncT<writeT, inT> swapConv);
+
+#define KUTILS_OVERLOAD_WRITER_IMPL(writerT, writeT, inT, func)                                                        \
+	template <>                                                                                                        \
+	void writerT::write<writeT, inT>(inT t, SwapConversionFuncT<writeT, inT> swapConv) BOOST_PP_REMOVE_PARENS(func)
+
+namespace khutils
+{
+}	// namespace khutils
+
+#endif	// ! KHUTILS_WRITER_OVERLOAD_HPP_INC
