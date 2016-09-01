1d251c82fc391607c283d8b48b4c4ea016e8817e
KageKirin <kagekirin+khutil@gmail.com>
2016-09-01 18:40:36 +0900

    added comprehensive headers for reader/writer overloads

---
+++ include/khutils/reader_glm.hpp
@@ -0,0 +1,8 @@
+﻿#ifndef KHUTILS_READER_GLM_HPP_INC
+#define KHUTILS_READER_GLM_HPP_INC
+
+#include "khutils/reader_be_glm.hpp"
+#include "khutils/reader_le_glm.hpp"
+#include "khutils/reader_ne_glm.hpp"
+
+#endif	// ! KHUTILS_READER_GLM_HPP_INC
---
+++ include/khutils/writer_glm.hpp
@@ -0,0 +1,8 @@
+﻿#ifndef KHUTILS_WRITER_GLM_HPP_INC
+#define KHUTILS_WRITER_GLM_HPP_INC
+
+#include "khutils/writer_be_glm.hpp"
+#include "khutils/writer_le_glm.hpp"
+#include "khutils/writer_ne_glm.hpp"
+
+#endif	// ! KHUTILS_WRITER_GLM_HPP_INC
