34d9b5018dbf1f720bb954fddae6d8be6e084839
KageKirin <kagekirin+khutil@gmail.com>
2016-10-04 12:50:02 +0900

    simplified pixel reader/writer pixel reader/writer NOT inheriting from memory reader/writer anymore removed superfluous code related to former inheritance

--- include/khutils/pixelreader.hpp
+++ include/khutils/pixelreader.hpp
@@ -4,71 +4,83 @@
 //! file has dependency on boost.endian
 //! include wisely to keep compile times minimal
 
-#include "khutils/memoryhandler.hpp"
-#include "khutils/typeconversion.hpp"
+#include "khutils/assertion.hpp"
 
-#include <boost/endian/conversion.hpp>
+#include <algorithm>
 #include <functional>
 #include <vector>
 
 namespace khutils
 {
-	using boost::endian::order;
-
 	//! data reader
 	//! read data from buffer
 	//! usage: use typedef'ed version (see below)
-	template <typename PixelType, typename ByteForwardIterator, order _order>
+	template <typename PixelType>
 	struct _pixelreader;
 
-	template <typename PixelType, typename ByteForwardIterator>
-	using pixelreader = _pixelreader<PixelType, ByteForwardIterator, order::native>;
-	template <typename PixelType, typename ByteForwardIterator>
-	using little_endian_pixelreader = _pixelreader<PixelType, ByteForwardIterator, order::little>;
-	template <typename PixelType, typename ByteForwardIterator>
-	using big_endian_pixelreader = _pixelreader<PixelType, ByteForwardIterator, order::big>;
+	template <typename PixelType>
+	using pixelreader = _pixelreader<PixelType>;
 
-	template <typename PixelType, typename ByteForwardIterator>
+	template <typename PixelType>
 	struct pixelreader_trait
 	{
-		typedef pixelreader<PixelType, ByteForwardIterator>				  native_endian_reader;
-		typedef little_endian_pixelreader<PixelType, ByteForwardIterator> little_endian_reader;
-		typedef big_endian_pixelreader<PixelType, ByteForwardIterator>	big_endian_reader;
+		typedef pixelreader<PixelType> native_endian_reader;
 	};
 
-	template <typename PixelType, typename ByteForwardIterator, order _order>
-	struct _pixelreader : protected _memoryreader<ByteForwardIterator _order>
+	template <typename PixelType>
+	struct _pixelreader
 	{
-		size_t m_width;
-		size_t m_height;
+		const PixelType* m_begin;
+		const PixelType* m_end;
+		size_t			 m_width;
+		size_t			 m_height;
 
 		_pixelreader()					  = delete;
 		_pixelreader(const _pixelreader&) = default;
 		_pixelreader(_pixelreader&&)	  = default;
-		_pixelreader(size_t				 width,
-					 size_t				 height,
-					 ByteForwardIterator begin,
-					 ByteForwardIterator end)	//
-		  : _memoryreader(begin, end),
+		_pixelreader(size_t			  width,
+					 size_t			  height,
+					 const PixelType* begin,
+					 const PixelType* end)	//
+		  : m_begin(begin),
+			m_end(end),
 			m_width(width),
 			m_height(height)
 		{
+			KHUTILS_ASSERT_EQUALS((m_width * m_height), (size_t)std::distance(m_begin, m_end));
 		}
 
 		_pixelreader& operator=(const _pixelreader&) = default;
 		_pixelreader& operator=(_pixelreader&&) = default;
 
-		PixelType peek(size_t offset)
+		PixelType peek(size_t offset) const
 		{
+			auto temp = (size_t)std::distance(begin(), end());
 			KHUTILS_ASSERT_EXPR(offset, Is().LessThan(m_width * m_height));
-			return fetchAt<PixelType>(offset);
+			KHUTILS_ASSERT_EXPR(offset, Is().LessThan(std::distance(begin(), end())));
+			return begin()[offset];
 		}
 
-		PixelType peekAt(size_t x, size_t y)
+		PixelType peekAt(size_t x, size_t y) const
 		{
 			KHUTILS_ASSERT_EXPR(x, Is().LessThan(m_width));
 			KHUTILS_ASSERT_EXPR(y, Is().LessThan(m_height));
 			return peek(y * m_width + x);
+		}
+
+		size_t pixelCount() const
+		{
+			return m_width * m_height;
+		}
+
+		const PixelType* begin() const
+		{
+			return m_begin;
+		}
+
+		const PixelType* end() const
+		{
+			return m_end;
 		}
 	};
 
--- include/khutils/pixelwriter.hpp
+++ include/khutils/pixelwriter.hpp
@@ -3,57 +3,47 @@
 
 //! file has dependency on boost.endian
 //! include wisely to keep compile times minimal
+#include "khutils/assertion.hpp"
 
-#include "khutils/memoryhandler.hpp"
-#include "khutils/typeconversion.hpp"
-
-#include <boost/endian/conversion.hpp>
 #include <functional>
 #include <vector>
 
 namespace khutils
 {
-	using boost::endian::order;
-
 	//! data writer
 	//! writes data into PRE-ALLOCATED buffer
 	//! DOES NOT RESIZE BUFFER
 	//! usage: use typedef'ed version (see below)
-	template <typename ByteForwardIterator, order _order>
+	template <typename PixelType>
 	struct _pixelwriter;
 
-	template <typename ByteForwardIterator>
-	using pixelwriter = _pixelwriter<ByteForwardIterator, order::native>;
-	template <typename ByteForwardIterator>
-	using little_endian_pixelwriter = _pixelwriter<ByteForwardIterator, order::little>;
-	template <typename ByteForwardIterator>
-	using big_endian_pixelwriter = _pixelwriter<ByteForwardIterator, order::big>;
+	template <typename PixelType>
+	using pixelwriter = _pixelwriter<PixelType>;
 
-	template <typename ByteForwardIterator>
+	template <typename PixelType>
 	struct pixelwriter_trait
 	{
-		typedef pixelwriter<ByteForwardIterator>			   native_endian_writer;
-		typedef little_endian_pixelwriter<ByteForwardIterator> little_endian_writer;
-		typedef big_endian_pixelwriter<ByteForwardIterator>	big_endian_writer;
+		typedef pixelwriter<PixelType> native_endian_writer;
 	};
 
-	template <typename ByteForwardIterator, order _order>
-	struct _pixelwriter : protected _memorywriter<ByteForwardIterator, _order>
+	template <typename PixelType>
+	struct _pixelwriter
 	{
-		size_t m_width;
-		size_t m_height;
+		PixelType* m_begin;
+		PixelType* m_end;
+		size_t	 m_width;
+		size_t	 m_height;
 
 		_pixelwriter()					  = delete;
 		_pixelwriter(const _pixelwriter&) = default;
 		_pixelwriter(_pixelwriter&&)	  = default;
-		_pixelwriter(size_t				 width,
-					 size_t				 height,
-					 ByteForwardIterator begin,
-					 ByteForwardIterator end)	//
-		  : _memorywriter(begin, end),
+		_pixelwriter(size_t width, size_t height, PixelType* begin, PixelType* end)	//
+		  : m_begin(begin),
+			m_end(end),
 			m_width(width),
 			m_height(height)
 		{
+			KHUTILS_ASSERT_EQUALS(m_width * m_height, (size_t)std::distance(m_begin, m_end));
 		}
 
 		_pixelwriter& operator=(const _pixelwriter&) = default;
@@ -62,14 +52,30 @@ namespace khutils
 		void poke(PixelType val, size_t offset)
 		{
 			KHUTILS_ASSERT_EXPR(offset, Is().LessThan(m_width * m_height));
-			return putAt<PixelType>(val, offset);
+			KHUTILS_ASSERT_EXPR(offset, Is().LessThan(std::distance(begin(), end())));
+			begin()[offset] = val;
 		}
 
 		void pokeAt(PixelType val, size_t x, size_t y)
 		{
 			KHUTILS_ASSERT_EXPR(x, Is().LessThan(m_width));
 			KHUTILS_ASSERT_EXPR(y, Is().LessThan(m_height));
-			return poke(val, y * m_width + x);
+			poke(val, y * m_width + x);
+		}
+
+		size_t pixelCount() const
+		{
+			return m_width * m_height;
+		}
+
+		PixelType* begin()
+		{
+			return m_begin;
+		}
+
+		PixelType* end()
+		{
+			return m_end;
 		}
 	};
 
