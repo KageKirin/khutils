909f8bb4a2cf1cf9194fe074046d0beeb0d16374
KageKirin <kagekirin+khutils@gmail.com>
2016-12-02 14:23:12 +0900

    added unitbuild files to create lib if necessary

---
+++ unitbuild/unitbuild.cpp
@@ -0,0 +1,57 @@
+﻿#define KHUTILS_ASSERTION_INLINE
+#define KHUTILS_ASSERTION_IMPL
+#define KHUTILS_COMPARISON_IMPL
+#define KHUTILS_FLATBUFFER_MAP_IMPL
+#define KHUTILS_FLATBUFFER_MAP_IMPL
+#define KHUTILS_FLATBUFFER_MULTIMAP_IMPL
+#define KHUTILS_FLATBUFFER_MULTIMAP_IMPL
+#define KHUTILS_LOGGING_IMPL
+#define KHUTILS_RUNTIME_EXCEPTIONS_IMPL
+#define KHUTILS_SPIRITUAL_CAST_IMPL
+#define KHUTILS_SPIRITUAL_COMPARE_IMPL
+#define KHUTILS_UTF8STRING_IMPL
+
+
+#include "khutils/algorithm_index.hpp"
+#include "khutils/assertion.hpp"
+#include "khutils/base_handler.hpp"
+#include "khutils/comparison.hpp"
+#include "khutils/filehandler.hpp"
+#include "khutils/filereader.hpp"
+#include "khutils/filewriter.hpp"
+#include "khutils/flatbuffer_container.hpp"
+#include "khutils/flatbuffer_map.hpp"
+#include "khutils/flatbuffer_multimap.hpp"
+#include "khutils/logging.hpp"
+#include "khutils/memoryhandler.hpp"
+#include "khutils/memoryreader.hpp"
+#include "khutils/memorywriter.hpp"
+#include "khutils/pixelalgorithm.hpp"
+#include "khutils/pixelhandler.hpp"
+#include "khutils/pixelreader.hpp"
+#include "khutils/pixelwriter.hpp"
+#include "khutils/reader_overload.hpp"
+#include "khutils/runtime_exceptions.hpp"
+#include "khutils/spiritual_cast.hpp"
+#include "khutils/spiritual_compare.hpp"
+#include "khutils/streamhandler.hpp"
+#include "khutils/streamreader.hpp"
+#include "khutils/streamwriter.hpp"
+#include "khutils/tlv.hpp"
+#include "khutils/tlvreader.hpp"
+#include "khutils/tlvwriter.hpp"
+#include "khutils/typeconversion.hpp"
+#include "khutils/utf8/utf8string.hpp"
+#include "khutils/writer_overload.hpp"
+
+
+//!! file.hpp and file_http.hpp conflict (on purpose!)
+//!! include either and define its _IMPL
+//!! you can use both, but as separate libs (e.g. dynamic)
+//!! Rationale: they expose the same API in order to allow the same code
+//!! to work with either local or remote (http) files
+//#define KHUTILS_FILE_IMPL
+//#include "khutils/file.hpp"
+
+//#define KHUTILS_FILE_HTTP_IMPL
+//#include "khutils/file_http.hpp"
---
+++ unitbuild/unitbuild_glm.cpp
@@ -0,0 +1,30 @@
+﻿#define KHUTILS_GLM_BIG_ENDIAN_READER_IMPL
+#define KHUTILS_GLM_BIG_ENDIAN_WRITER_IMPL
+#define KHUTILS_GLM_LITTLE_ENDIAN_READER_IMPL
+#define KHUTILS_GLM_LITTLE_ENDIAN_WRITER_IMPL
+//!! native reader/writer conflicts with either big or little implementation
+//!! reason: boost enum for endian::native takes either value
+//#define KHUTILS_GLM_NATIVE_ENDIAN_READER_IMPL
+//#define KHUTILS_GLM_NATIVE_ENDIAN_WRITER_IMPL
+#define KHUTILS_GLM_COMPARISON_IMPL
+#define KHUTILS_GLM_ISNAN_IMPL
+#define KHUTILS_GLM_TO_FBX_IMPL
+#define KHUTILS_GLM_TO_STRING_IMPL
+#define KHUTILS_GLM_UTILITY_IMPL
+
+
+#include "khutils/glm/glm_big_endian_reader.hpp"
+#include "khutils/glm/glm_big_endian_writer.hpp"
+#include "khutils/glm/glm_comparison.hpp"
+#include "khutils/glm/glm_endian_reverse.hpp"
+#include "khutils/glm/glm_euler_angles.hpp"
+#include "khutils/glm/glm_isnan.hpp"
+#include "khutils/glm/glm_little_endian_reader.hpp"
+#include "khutils/glm/glm_little_endian_writer.hpp"
+#include "khutils/glm/glm_native_endian_reader.hpp"
+#include "khutils/glm/glm_native_endian_writer.hpp"
+#include "khutils/glm/glm_reader.hpp"
+#include "khutils/glm/glm_to_fbx.hpp"
+#include "khutils/glm/glm_to_string.hpp"
+#include "khutils/glm/glm_utility.hpp"
+#include "khutils/glm/glm_writer.hpp"
