b3ca4dabbacd76878cd8f10affbbfd568714f18c
kagekirin@gmail.com <christian.helmich@dena.com>
2017-04-28 18:48:40 +0900

    file_http using bnet instead of boost for HTTP access

--- include/khutils/file_http.hpp
+++ include/khutils/file_http.hpp
@@ -14,8 +14,11 @@ namespace khutils
 	std::string getServerPath(const std::string& url);
 	std::string getServerPathHash(const std::string& url);
 	std::string getServerPathWithHash(const std::string& url);
+	std::string urlToHttpRequestHeader(const std::string& url);
 
 	void openHttpSocket(boost::asio::ip::tcp::iostream& s, const std::string& url);
+
+
 	std::stringstream openHttpFile(const std::string& url);
 	std::vector<uint8_t> openHttpFileBuffer(const std::string& url);
 
@@ -31,6 +34,11 @@ namespace khutils
 
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/filesystem/path.hpp>
+
+#include "bx/string.h"
+#include "bnet/bnet.h"
+#include "common/url.h"
+
 
 #include <cassert>
 #include <regex>
@@ -51,44 +59,86 @@ namespace khutils
 
 	std::string getServerName(const std::string& url)
 	{
-		std::smatch match;
-		bool		matchOK = std::regex_match(url, match, reURI);
-		assert(matchOK);
-		assert(match.size() > 4);
+		char* tokens[UrlToken::Count];
+		char  temp[1024];
+		tokenizeUrl(url.c_str(), temp, sizeof(temp), tokens);
 
-		return match[4];
+		return tokens[UrlToken::Host];
 	}
+
+	//------------------------------------------
 
 	std::string getServerPath(const std::string& url)
 	{
-		std::smatch match;
-		bool		matchOK = std::regex_match(url, match, reURI);
-		assert(matchOK);
-		assert(match.size() > 5);
+		char* tokens[UrlToken::Count];
+		char  temp[1024];
+		tokenizeUrl(url.c_str(), temp, sizeof(temp), tokens);
 
-		return match[5];
+		return tokens[UrlToken::Path];
 	}
+
+	//------------------------------------------
 
 	std::string getServerPathHash(const std::string& url)
 	{
-		std::smatch match;
-		bool		matchOK = std::regex_match(url, match, reURI);
-		assert(matchOK);
-		assert(match.size() > 9);
+		char* tokens[UrlToken::Count];
+		char  temp[1024];
+		tokenizeUrl(url.c_str(), temp, sizeof(temp), tokens);
 
-		return match[9];
+		return tokens[UrlToken::Query];
 	}
+
+	//------------------------------------------
 
 	std::string getServerPathWithHash(const std::string& url)
 	{
-		std::smatch match;
-		bool		matchOK = std::regex_match(url, match, reURI);
-		assert(matchOK);
-		assert(match.size() > 9);
+		char* tokens[UrlToken::Count];
+		char  temp[1024];
+		tokenizeUrl(url.c_str(), temp, sizeof(temp), tokens);
 
-		return match[5].str() + match[8].str();
+		return std::string(tokens[UrlToken::Path]) + tokens[UrlToken::Query];
 	}
 
+	//------------------------------------------
+
+	std::string urlToHttpRequestHeader(const std::string& url)
+	{
+		using namespace fmt::literals;
+
+		char* tokens[UrlToken::Count];
+		char  temp[1024];
+		tokenizeUrl(url.c_str(), temp, sizeof(temp), tokens);
+
+		return "GET /{} HTTP/1.0\r\nHost: {}\r\n\r\n"_format(tokens[UrlToken::Path], tokens[UrlToken::Host]);
+	}
+
+	//------------------------------------------
+
+	std::tuple<uint32_t, uint16_t, bool> getServerIPPort(const std::string& url)
+	{
+		char* tokens[UrlToken::Count];
+		char  temp[1024];
+		tokenizeUrl(url.c_str(), temp, sizeof(temp), tokens);
+
+		bool	 secure = false;
+		uint16_t port   = 80;
+
+		if (0 == bx::strCmpI(tokens[UrlToken::Scheme], "https"))
+		{
+			port   = 443;
+			secure = true;
+		}
+
+		if (tokens[UrlToken::Port][0] != '\0')
+		{
+			port = std::atoi(tokens[UrlToken::Port]);
+		}
+
+		return {bnet::toIpv4(tokens[UrlToken::Host]), port, secure};
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////
 	//! modeled after
 	//! http://www.boost.org/doc/libs/1_47_0/doc/html/boost_asio/example/iostreams/http_client.cpp
 	//! only work on http, NOT https
@@ -146,9 +196,97 @@ namespace khutils
 		}
 	}
 
+	//------------------------------------------
+
 	//////////////////////////////////////////////////////////////////////////
 
-	std::stringstream openHttpFile(const std::string& url)
+	bnet::Handle httpSendRequest(uint32_t _ip, uint16_t _port, const char* _request, bool secure)
+	{
+		bnet::Handle handle = bnet::connect(_ip, _port, true, secure);
+
+		bnet::Message* out = bnet::alloc(handle, (uint16_t)strlen(_request));
+		bx::memCopy(out->data, _request, out->size);
+		bnet::send(out);
+		bnet::notify(handle, UINT64_C(0x123456789ABCDEF));
+
+		return handle;
+	}
+
+	//------------------------------------------
+
+	bnet::Handle httpSendFileRequest(const std::string& url)
+	{
+		uint32_t ip;
+		uint16_t port;
+		bool	 secure;
+		std::tie(ip, port, secure) = getServerIPPort(url);
+
+		return httpSendRequest(ip, port, urlToHttpRequestHeader(url).c_str(), secure);
+	}
+
+	//------------------------------------------
+
+	std::vector<uint8_t> openHttpFileBuffer(const std::string& url)
+	{
+		if(static bool once = true; once)
+		{
+			bnet::init(1, 0, nullptr);
+			once = false;
+		}
+		
+		auto handle = httpSendFileRequest(url);
+		KHUTILS_ASSERT(bnet::isValid(handle));
+
+		std::vector<uint8_t> buffer;
+
+		bool cont = bnet::isValid(handle); 
+		while (cont)
+		{
+			bnet::Message* msg = bnet::recv();
+			if (NULL != msg)
+			{
+				if (bnet::MessageId::UserDefined > msg->data[0])
+				{
+					switch (msg->data[0])
+					{
+						case bnet::MessageId::Notify:
+						{
+							khutils::logger::error() << "notify";
+						}
+						break;
+
+						case bnet::MessageId::LostConnection:
+						{
+							cont = false;
+							khutils::logger::error() << "lost connection";
+						}
+						break;
+
+						case bnet::MessageId::ConnectFailed:
+						{
+							cont = false;
+							khutils::logger::error() << "connection failed";
+						}
+						break;
+
+						case bnet::MessageId::RawData:
+						{
+							uint8_t* start = &msg->data[1];
+							uint8_t* end   = start + (msg->size - 1);
+							std::copy(start, end, std::back_inserter(buffer));
+						}
+						break;
+					}
+				}
+				bnet::release(msg);
+			}
+		}
+		return buffer;
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+
+	std::stringstream openHttpFile_boost(const std::string& url)
 	{
 		ip::tcp::iostream s;
 		openHttpSocket(s, url);
@@ -159,7 +297,9 @@ namespace khutils
 		return ret;
 	}
 
-	std::vector<uint8_t> openHttpFileBuffer(const std::string& url)
+	//------------------------------------------
+
+	std::vector<uint8_t> openHttpFileBuffer_boost(const std::string& url)
 	{
 		ip::tcp::iostream s;
 		openHttpSocket(s, url);
@@ -175,6 +315,8 @@ namespace khutils
 		}
 		return ret;
 	}
+
+	//------------------------------------------
 
 
 	//////////////////////////////////////////////////////////////////////////
