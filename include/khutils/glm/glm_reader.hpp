b03a17a20e6e24cbf2686d36d45197603844c533
kagekirin@gmail.com <christian.helmich@dena.com>
2017-06-07 11:23:54 +0900

    removed obsolete files

--- include/khutils/glm/glm_reader.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef KHUTILS_GLM_READER_HPP_INC
-#define KHUTILS_GLM_READER_HPP_INC
-
-#include "khutils/glm/glm_big_endian_reader.hpp"
-#include "khutils/glm/glm_little_endian_reader.hpp"
-#include "khutils/glm/glm_native_endian_reader.hpp"
-
-#endif	// ! KHUTILS_GLM_READER_HPP_INC
--- include/khutils/glm/glm_writer.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef KHUTILS_GLM_WRITER_HPP_INC
-#define KHUTILS_GLM_WRITER_HPP_INC
-
-#include "khutils/glm/glm_big_endian_writer.hpp"
-#include "khutils/glm/glm_little_endian_writer.hpp"
-#include "khutils/glm/glm_native_endian_writer.hpp"
-
-#endif	// ! KHUTILS_GLM_WRITER_HPP_INC
